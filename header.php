4f0f3b6f1c5de509076da44b92e6b503ef16d22c
Ruud Slagers <ruudslagers@gmail.com>
2021-06-15 16:13:26 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Complement_Reclame
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'complement' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="wrapper site-header__wrapper">
+			<?php the_custom_logo(); ?>
+				<a href="<?php echo home_url(); ?>">
+                    <img class="brand" src="/wp-content/uploads/2021/06/ComplementReclame_Logo.svg" alt="Complement Reclame">
+                </a>
+		</div><!-- .site-header__wrapper -->
+
+		<nav id="site-navigation" class="main-navigation">
+			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'complement' ); ?></button>
+			<?php
+			wp_nav_menu(
+				array(
+					'theme_location' => 'menu-1',
+					'menu_id'        => 'primary-menu',
+				)
+			);
+			?>
+		</nav><!-- #site-navigation -->
+
+		<a href="#" class="button">Contact</a>
+
+	</header><!-- #masthead -->
